e8f89b2ec3bcdbc4f8deb74a90d0b40ce9d248c0
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-09-03 17:19:50 +0100

    Added example of try: except

--- control_flow.py
+++ control_flow.py
@@ -37,3 +37,21 @@ def cheapest_option(weight):
 
 cheapest_option(4.8)
 cheapest_option(41.5)
+
+
+def divide_two_numbers(x, y):
+    result = x / y
+    return result
+
+
+try:
+    result = divide_two_numbers(2, 0)
+    print(result)
+except NameError:
+    print("A NameError occurred.")
+except ValueError:
+    print("A ValueError occurred.")
+except ZeroDivisionError:
+    print("A ZeroDivisionError occurred.")
+
+# try: except blocks can be used to catch errors so above we will get "A ZeroDivisionError occurred." printed
