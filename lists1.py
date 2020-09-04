0e6ffb37c269d6c50e063258bfabc89993fb0d9d
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-09-04 10:27:58 +0100

    Added section about range() to lists1.py

--- lists1.py
+++ lists1.py
@@ -16,3 +16,14 @@ print(gradebook)
 full_gradebook = last_semester_gradebook + gradebook
 # we can also combine two lists together with + this also works for [98, 97, 85, 88] + [10] = [98, 97, 85, 88, 10]
 print(full_gradebook)
+
+
+my_range = range(11)
+# makes a range object from 0 up to but not including 11
+print(list(my_range))
+# turns the range object into a list and prints [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+my_range_two = range(5, 101, 10)
+# range can take up to 3 arguments 5 is where we start, 101 is where we end and 10 is by how much we increment each time
+print(list(my_range_two))
+# this will output [5, 15, 25, 35, 45, 55, 65, 75, 85, 95]
