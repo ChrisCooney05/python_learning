e9bd9e5da46b8c94f859c9dc5838c37dc74b9c32
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-09-08 11:21:25 +0100

    finished

--- files_py/script.py
+++ files_py/script.py
@@ -24,3 +24,27 @@ with open('./files_py/boss_message.json', 'w') as boss_message:
     }
     json.dump(boss_message_dict, boss_message)
 # Makes a json file or overwrites if it already exists, dictionairy is the first argument file target is the second
+
+with open('./files_py/passwords.csv', 'a') as new_passwords_obj:
+    slash_null_sig = '''
+   _  _     ___   __  ____             
+/ )( \   / __) /  \(_  _)            
+) \/ (  ( (_ \(  O ) )(              
+\____/   \___/ \__/ (__)             
+ _  _   __    ___  __ _  ____  ____  
+/ )( \ / _\  / __)(  / )(  __)(    \ 
+) __ (/    \( (__  )  (  ) _)  ) D ( 
+\_)(_/\_/\_/ \___)(__\_)(____)(____/ 
+        ____  __     __   ____  _  _ 
+ ___   / ___)(  )   / _\ / ___)/ )( \
+(___)  \___ \/ (_/\/    \\___ \) __ (
+       (____/\____/\_/\_/(____/\_)(_/
+ __ _  _  _  __    __                
+(  ( \/ )( \(  )  (  )               
+/    /) \/ (/ (_/\/ (_/\             
+\_)__)\____/\____/\____/
+'''
+    new_passwords_obj.write(slash_null_sig)
+
+# Running this file once more with 'w' will delete the passwords.csv and replace it with our slash_null_sig string.
+# using 'a' as an argument to show how it works
