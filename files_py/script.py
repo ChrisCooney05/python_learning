7d31da3251378763914eed84fc43de59021223c9
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-09-08 11:18:59 +0100

    Able to produce a JSON file using python command

--- files_py/script.py
+++ files_py/script.py
@@ -1,4 +1,5 @@
 import csv
+import json
 
 compromised_users = []
 
@@ -15,3 +16,11 @@ with open('./files_py/compromised_users.txt', 'w') as compromised_user_file:
 # Not including the '\n' produces one long ugly string
 # This will overwrite the file if it exists or make a new one if it does not. 'w' stands for write, if we want to add
 # to the file we can use 'a' for append.
+
+with open('./files_py/boss_message.json', 'w') as boss_message:
+    boss_message_dict = {
+        'recipient': 'The Boss',
+        'Message': 'Mission Success'
+    }
+    json.dump(boss_message_dict, boss_message)
+# Makes a json file or overwrites if it already exists, dictionairy is the first argument file target is the second
